a2f4944ca6cfd7f59755ec9a84940dd10eada303
lakshankpg <lakshankpg@gmail.com>
2025-07-03 18:48:04 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,19 +1,11 @@
 
-//_____ _    _ _      _    __  __ ____  
- // / ____| |  | | |    / \  |  \/  |  _ \ 
- //| (___ | |  | | |   / _ \ | |\/| | | | |
- // \___ \| |  | | |  / ___ \| |  | | |_| |
- // ____) | |__| | |_/ /   \ \_|  |_|____/ 
- //|_____/ \____/|_____/     \_\          
- 
-//             S U L A - M D
 
 import * as mega from 'megajs';
 
 // Mega authentication credentials
 const auth = {
-    email: 'ඔයාගේ ඊමේල්', // Replace with your Mega email
-    password: 'ඔයාගේ පාස්වර්ඩ්', // Replace with your Mega password
+    email: 'premasirimalith20@gmail.com', // Replace with your Mega email
+    password: 'lakshankpg17', // Replace with your Mega password
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
